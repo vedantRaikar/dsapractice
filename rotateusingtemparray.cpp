d41839094f1cbdd3539cd00edee1d942ab111981
vedantRaikar <vedantraikar117@gmail.com>
2023-08-10 15:07:58 +0530

    arrayrotation

--- rotateusingtemparray.cpp
+++ rotateusingtemparray.cpp
@@ -1,7 +1,7 @@
 //
 // Created by vedant raikar on 10-08-2023.
 //
-#include<bits.stdc++.h>
+#include<bits/stdc++.h>
 using namespace std ;
 void print(int arr[] , int n ){
     for(int i = 0 ; i < n ; i++){
@@ -9,12 +9,29 @@ void print(int arr[] , int n ){
     }
 }
 void rotate(int arr[] , int n , int d){
+int temp[n];
+int j = 0 ;
 
+for( int i = d ; i < n ; i++){
+    temp[j] = arr[i];
+    j++;
 }
+
+for( int i = 0 ; i < d ; i++){
+    temp[j] = arr[i];
+}
+
+for ( int i = 0 ; i < n ; i++){
+    arr[i] = temp[i] ;
+}
+}
+
+
 int main (){
 int arr[] = {1,2,3,4,5};
 int n = sizeof(arr) / sizeof(arr[0]);
-int d = 2
-        
+int d = 2 ;
+rotate( arr , n , d);
+print( arr , n);
 }
 
